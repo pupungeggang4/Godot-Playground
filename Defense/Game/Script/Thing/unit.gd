390e4878f23974cf435c73ad03064187be971057
pupungeggang6 <pupungeggang6@gmail.com>
2025-03-09 22:39:58 +0900

    HP

--- Defense/Game/Script/Thing/unit.gd
+++ Defense/Game/Script/Thing/unit.gd
@@ -4,17 +4,23 @@ extends Node2D
 @export var hp_max = 20.0
 @export var speed = 200.0
 
+@onready var node_hp_bar = get_node("HPBarFull")
+
 func _ready():
-    pass
+	pass
 
 func _process(delta: float):
-    move(delta)
-    handle_death()
+	move(delta)
+	handle_death()
+	handle_hp_bar()
 
 func move(delta):
-    position.x -= speed * delta
+	position.x -= speed * delta
+
+func handle_hp_bar():
+	node_hp_bar.size.x = hp / hp_max * 64.0
 
 func handle_death():
-    hp -= 0.1
-    if hp <= 0:
-        queue_free()
+	hp -= 0.1
+	if hp <= 0:
+		queue_free()
