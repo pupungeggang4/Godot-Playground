b336850975eff30cf22b4aa0b91fb58caf440aaa
pupungeggang6 <pupungeggang6@gmail.com>
2025-04-05 15:04:42 +0900

    Reset

--- Defense/DevNote/TodoList.md
+++
@@ -1,7 +0,0 @@
-# Todo List
-
-## Issue
-
-- [ ] More information while unit spawn.
-- [ ] Card playing mechanism.
-- [ ] Collide and attack handle.
--- Defense/Game/Script/physics.gd
+++
@@ -1,7 +0,0 @@
-extends Node
-
-func point_inside_rect(point, rect):
-    return point.x > rect.position.x and point.x < rect.position.x + rect.size.x and point.y > rect.position.y and point.y < rect.position.y + rect.size.y
-
-func point_inside_rect_ui(point, rect):
-    return point.x > rect[0] and point.x < rect[0] + rect[2] and point.y > rect[1] and point.y < rect[1] + rect[3]
