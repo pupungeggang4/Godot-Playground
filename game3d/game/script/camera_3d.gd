eb2070dcd34ec24c2492ce2bec00734b015100a4
pupungeggang6 <pupungeggang6@gmail.com>
2025-07-26 18:57:04 +0900

    Temp

--- game3d/game/script/camera_3d.gd
+++ game3d/game/script/camera_3d.gd
@@ -3,6 +3,8 @@ extends Camera3D
 @onready var node_kart = get_node('../Kart')
 
 func _process(delta):
-    position.x = node_kart.position.x
+    var back = Vector3.FORWARD.rotated(Vector3.UP, node_kart.rotate_angle)
+    position.x = node_kart.position.x + back.x * 2.0
     position.y = node_kart.position.y + 1.0
-    position.z = node_kart.position.z - 2.0
+    position.z = node_kart.position.z + back.z * 2.0
+    rotation.y = node_kart.rotate_angle + PI
--- game3d/game/script/kart.gd
+++ game3d/game/script/kart.gd
@@ -1,17 +1,27 @@
 extends Node3D
 
 @export var speed = 0.0
-@export var speed_max = 5.0
-@export var acceler = 2.0
-@export var frict = 2.0
+@export var speed_max = 2.5
+@export var acceler = 3.0
+@export var frict = 5.0
+@export var foward = Vector3(0.0, 0.0, 1.0)
+@export var foward_world = Vector3(0.0, 0.0, 1.0)
+@export var rotate_angle = 0.0
+@export var handle = 1.0
 
 func _process(delta: float) -> void:
     if Input.is_action_pressed('foward'):
+        if Input.is_action_pressed('left'):
+            rotate_angle += delta * handle
+        elif Input.is_action_pressed('right'):
+            rotate_angle -= delta * handle
+
         speed += acceler * delta
     else:
         if speed > 0:
             speed -= frict * delta
         else:
             speed = 0
-        
-    position.z += speed * delta
+    
+    position += foward_world.rotated(Vector3.UP, rotate_angle) * speed * delta
+    rotation.y = rotate_angle
