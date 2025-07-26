53e869a4528a88c1714e8d80415782816da6986f
pupungeggang2 <kyongkyong2892@gmail.com>
2025-07-26 23:29:36 +0900

    Track

--- game3d/game/script/kart.gd
+++ game3d/game/script/kart.gd
@@ -1,8 +1,8 @@
 extends Node3D
 
 @export var speed = 0.0
-@export var speed_max = 2.5
-@export var acceler = 3.0
+@export var speed_max = 30.0
+@export var acceler = 8.0
 @export var frict = 5.0
 @export var foward = Vector3(0.0, 0.0, 1.0)
 @export var foward_world = Vector3(0.0, 0.0, 1.0)
@@ -10,18 +10,19 @@ extends Node3D
 @export var handle = 1.0
 
 func _process(delta: float) -> void:
-    if Input.is_action_pressed('foward'):
-        if Input.is_action_pressed('left'):
-            rotate_angle += delta * handle
-        elif Input.is_action_pressed('right'):
-            rotate_angle -= delta * handle
-
-        speed += acceler * delta
-    else:
-        if speed > 0:
-            speed -= frict * delta
-        else:
-            speed = 0
-    
-    position += foward_world.rotated(Vector3.UP, rotate_angle) * speed * delta
-    rotation.y = rotate_angle
+	if Input.is_action_pressed('foward') and speed < speed_max:
+		speed += acceler * delta
+	else:
+		if speed > 0:
+			speed -= frict * delta
+		else:
+			speed = 0
+			
+	if speed > 0:
+		if Input.is_action_pressed('left'):
+			rotate_angle += delta * handle
+		elif Input.is_action_pressed('right'):
+			rotate_angle -= delta * handle
+	
+	position += foward_world.rotated(Vector3.UP, rotate_angle) * speed * delta
+	rotation.y = rotate_angle
