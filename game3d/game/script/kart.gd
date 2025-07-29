56b2d77afce6b428964df1139f95e91fd82148b5
pupungeggang2 <kyongkyong2892@gmail.com>
2025-07-29 13:07:43 +0900

    Temp

--- game3d/game/script/kart.gd
+++ game3d/game/script/kart.gd
@@ -10,23 +10,23 @@ extends Node3D
 @export var handle = 1.0
 
 func _process(delta: float) -> void:
-    if Input.is_action_pressed('foward') and speed < speed_max:
-        speed += acceler * delta
-    else:
-        if speed > 0:
-            speed -= frict * delta
-        else:
-            speed = 0
-            
-    if speed > 0:
-        if Input.is_action_pressed('left'):
-            rotate_angle += delta * handle
-        elif Input.is_action_pressed('right'):
-            rotate_angle -= delta * handle
-    
-    position += foward_world.rotated(Vector3.UP, rotate_angle) * speed * delta
-    rotation.y = rotate_angle
-    
-    if speed > 0:
-        get_node('Wheel1').rotation.x += speed * 0.5 * delta
-        get_node('Wheel2').rotation.x += speed * 0.5 * delta
+	if Input.is_action_pressed('foward') and speed < speed_max:
+		speed += acceler * delta
+	else:
+		if speed > 0:
+			speed -= frict * delta
+		else:
+			speed = 0
+			
+	if speed > 0:
+		if Input.is_action_pressed('left'):
+			rotate_angle += delta * handle
+		elif Input.is_action_pressed('right'):
+			rotate_angle -= delta * handle
+	
+	position += foward_world.rotated(Vector3.UP, rotate_angle) * speed * delta
+	rotation.y = rotate_angle
+	
+	if speed > 0:
+		get_node('Wheel1').rotation.x += speed * 0.5 * delta
+		get_node('Wheel2').rotation.x += speed * 0.5 * delta
