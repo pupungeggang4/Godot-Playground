4e53610f6249dc438fed07d5ac515ff50d4aab22
pupungeggang6 <pupungeggang6@gmail.com>
2025-09-05 14:15:25 +0900

    Start

--- cardadventure/game/script/scene/field.gd
+++ cardadventure/game/script/scene/field.gd
@@ -1,6 +1,25 @@
 extends Node2D
 
 func _process(delta):
+    handle_input()
+    
+func handle_input():
+    handle_mouse()
+    handle_key()
+
+func handle_mouse():
     if Input.is_action_just_released('mouse_left'):
         var mouse = get_viewport().get_mouse_position()
-        print(mouse)
+        if GVar.menu == false:
+            if Func.point_inside_rect_ui(mouse, UI.button_menu):
+                GVar.menu = true
+                get_node('UI/FieldMenu').show()
+            if GVar.state == '':
+                pass
+        else:
+            if Func.point_inside_rect_ui(mouse, UI.buttton_resume):
+                GVar.menu = false
+                get_node('UI/FieldMenu').hide()
+    
+func handle_key():
+    pass
