d90122342e79fc65fa4cb9b59088320995c9c75e
pupungeggang6 <pupungeggang6@gmail.com>
2025-09-15 14:31:31 +0900

    Fix Code

--- defense/game/script/scene/title.gd
+++ defense/game/script/scene/title.gd
@@ -1,6 +1,12 @@
 extends Node2D
 
 func _process(delta):
+    handle_input()
+
+func handle_input():
+    handle_mouse()
+
+func handle_mouse():
     if Input.is_action_just_released('mouse_left'):
         var mouse = get_viewport().get_mouse_position()
         if Func.point_inside_rect_ui(mouse, [160, 160, 960, 80]):
